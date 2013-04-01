e353b11e851a33a733642031e3122e5602348d9f
Vivek <vivek.thuravupala@sourcebits.com>
2013-04-01 18:45:07 +0530

    Included basic assets

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<title>snapNslide! :D</title>
+	<link rel="stylesheet" href="assets/CSS/reset.css" />
 	<link rel="stylesheet" href="assets/CSS/snapNslide.css" />
 	<script src="assets/JS/jquery-1.8.3.min.js"></script>
 	<script src="assets/JS/snapNslide.js"></script>
@@ -9,7 +10,17 @@
 	<div id="snapNslide">
 		<div class="nav_prev"></div>
 		<div class="slideGuide">
-
+			<ul>
+				<li class="terminalNode_slot"></li>
+				<li class="mainNode_slot"></li>
+				<li class="mainNode_slot"></li>
+				<li class="mainNode_slot"></li>
+				<li class="mainNode_slot"></li>
+				<li class="mainNode_slot"></li>
+				<li class="mainNode_slot"></li>
+				<li class="terminalNode_slot"></li>
+			</ul>
+			<div class="terminalNode_tab"></div>
 		</div>
 		<div class="nav_next"></div>
 	</div>
